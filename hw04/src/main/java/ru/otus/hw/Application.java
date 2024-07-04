2f3e7557c9c91c1aacb1edaed0f417eba89e3cc4
redbeardbit <ai-ai@nxt.ru>
2024-07-04 21:47:26 +0300

    Добавил файл README.md

--- hw04/src/main/java/ru/otus/hw/Application.java
+++ hw04/src/main/java/ru/otus/hw/Application.java
@@ -1,12 +1,28 @@
 package ru.otus.hw;
 
+
+import org.jline.utils.AttributedString;
+import org.jline.utils.AttributedStyle;
+
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.shell.command.annotation.CommandScan;
+import org.springframework.shell.jline.PromptProvider;
+
 
 @SpringBootApplication
+@CommandScan
 public class Application {
-    public static void main(String[] args) {
-        SpringApplication application = new SpringApplication(Application.class);
-        application.run(args);
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+        // TODO: follow up with boot why spring.main.banner-mode=off doesn't work
+        // SpringApplication.run(SpringShellSample.class, args);
+    }
+
+    @Bean
+    public PromptProvider myPromptProvider() {
+        return () -> new AttributedString("otus:>", AttributedStyle.DEFAULT.foreground(AttributedStyle.YELLOW));
     }
 }
